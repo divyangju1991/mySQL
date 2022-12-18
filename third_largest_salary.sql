474421f2c3bb4c079835eb6274b25fd9a1b4f756
divyangju1991 <51605929+divyangju1991@users.noreply.github.com>
2022-12-18 19:42:59 +0530

    SQL

--- third_largest_salary.sql
+++ third_largest_salary.sql
@@ -7,4 +7,4 @@ SELECT e.*, MAX(e.salary) from employee e GROUP BY e.department, e.id;
 
 ALTER TABLE employee ADD column department VARCHAR(30);
 
-SELECT e.* FROM employee e WHERE (e.department,e.salary) IN (SELECT e1.department, MAX(e1.salary) FROM employee e1 GROUP BY e1.department)
+SELECT e.* FROM employee e WHERE (e.department,e.salary) IN (SELECT e1.department, MAX(e1.salary) FROM employee e1 GROUP BY e1.department);
