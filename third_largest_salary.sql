e3cb748dc9db8696f08d7fe4874093ad82944f11
divyangju1991 <51605929+divyangju1991@users.noreply.github.com>
2022-12-18 19:40:46 +0530

    SQL

--- third_largest_salary.sql
+++ third_largest_salary.sql
@@ -2,3 +2,9 @@ SELECT * FROM employee;
 
 Select salary from employee order by salary DESC LIMIT 2,1;
 #5300.5
+
+SELECT e.*, MAX(e.salary) from employee e GROUP BY e.department, e.id;
+
+ALTER TABLE employee ADD column department VARCHAR(30);
+
+SELECT e.* FROM employee e WHERE (e.department,e.salary) IN (SELECT e1.department, MAX(e1.salary) FROM employee e1 GROUP BY e1.department)
